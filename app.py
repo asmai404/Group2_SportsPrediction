311d399e55c24fba98ac663b60904520387c352b
asmai404 <148724088+asmai404@users.noreply.github.com>
2023-10-23 23:07:13 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+import numpy as np
 from flask import Flask, render_template, request
-# import pandas as pd
-# from sklearn.impute import SimpleImputer
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.ensemble import GradientBoostingRegressor
-# from sklearn.ensemble import RandomForestRegressor
-# import xgboost as xgb
-# from sklearn.model_selection import GridSearchCV, KFold
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, KFold
 import pickle
 
 app = Flask(__name__)
 
-# Load your trained model from a pickle file
-# with open('football_prediction_model.pkl', 'rb') as model_file:
-#     model = pickle.load(model_file)
+# Loading trained model from a pickle file
+with open('football_prediction_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,15 +26,45 @@ def index():
         mentality_composure = float(request.form['mentality_composure'])
         dribbling = float(request.form['dribbling'])
         potential = float(request.form['potential'])
+        wage_eur = float(request.form['wage_eur'])
         release_clause_eur = float(request.form['release_clause_eur'])
+        value_eur = float(request.form['value_eur'])
+        power_shot_power = float(request.form['power_shot_power'])
+        physic = float(request.form['physic'])
+        mentality_vision = float(request.form['mentality_vision'])
+        attacking_short_passing = float(request.form['attacking_short_passing'])
 
         # Create a feature vector with the input data
-        input_data = [movement_reactions, passing, mentality_composure, dribbling, potential, release_clause_eur]
+        input_data = {
+            "movement_reactions":[movement_reactions],
+            "passing": [passing],
+            "mentality_composure": [mentality_composure],
+            "dribbling": [dribbling],
+            "potential": [potential],
+            "release_clause_eur": [release_clause_eur],
+            "wage_eur": [wage_eur],
+            "value_eur": [value_eur],
+            "power_shot_power": [power_shot_power],
+            "physic":[physic],
+            "mentality_vision": [mentality_vision],
+            "attacking_short_passing": [attacking_short_passing],
+
+        }
+
+        # print(list(input_data.values()))
+        # sc = StandardScaler()
+        # scaled = sc.fit_transform(list(input_data.values()))
+        # print(scaled)
+
+
+        input_data_scaled = pd.DataFrame(input_data, columns=input_data.keys())
+
+        
 
         # Make predictions using your model
 
-        result = sum(input_data) / len(input_data) + 15 * 0.7
-        # result = model.predict([input_data])
+        # result = sum(input_data) / len(input_data) + 15 * 0.7
+        result = model.predict(input_data_scaled)
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
